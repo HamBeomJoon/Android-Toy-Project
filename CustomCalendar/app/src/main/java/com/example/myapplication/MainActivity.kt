cddb74610e60a9d12e538f84928744a6a2a10efe
HamBeomJoon <hbj0802@naver.com>
2024-07-19 12:10:49 +0900

    project 세팅

--- CustomCalendar/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ CustomCalendar/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.example.myapplication.util.BindingActivity
 
-class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val monthListManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val monthListAdapter = AdapterMonth()
+
+        binding.calendarCustom.apply {
+            layoutManager = monthListManager
+            adapter = monthListAdapter
+            scrollToPosition(Int.MAX_VALUE / 2)
+        }
+
+        val snap = PagerSnapHelper()
+        snap.attachToRecyclerView(binding.calendarCustom)
     }
 }
