0848b260f1fc0874c594c79a32196a9d08010b9c
Ham BeomJoon <hbj0802@naver.com>
2025-06-16 12:55:24 +0900

    feat: init Inset, EdgeToEdge 적용

--- GithubSearch/app/src/main/java/com/example/app/presentation/view/BaseActivity.kt
+++ GithubSearch/app/src/main/java/com/example/app/presentation/view/BaseActivity.kt
@@ -1,8 +1,12 @@
 package com.example.app.presentation.view
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
@@ -13,14 +17,24 @@ abstract class BaseActivity<T : ViewDataBinding>(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         beforeSetContentView()
-
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, layoutId)
         binding.lifecycleOwner = this
 
+        enableEdgeToEdge()
+        initInsets(binding.root)
         initView()
         initViewModel()
         initListener()
+    }
+
+    private fun initInsets(targetView: View) {
+        ViewCompat.setOnApplyWindowInsetsListener(targetView) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 
     protected open fun beforeSetContentView() {}
