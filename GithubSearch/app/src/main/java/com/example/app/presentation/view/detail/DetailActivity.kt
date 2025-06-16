7d4f84998b72088663149691aa24770f5a1241cb
Ham BeomJoon <hbj0802@naver.com>
2025-06-17 01:07:38 +0900

    refactor: 유저 상세 페이지 activity로 변경

---
+++ GithubSearch/app/src/main/java/com/example/app/presentation/view/detail/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.app.presentation.view.detail
+
+import com.example.app.R
+import com.example.app.databinding.ActivityMainBinding
+import com.example.app.presentation.view.BaseActivity
+
+class DetailActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_detail) {
+    override fun initView() {
+        super.initView()
+    }
+}
--- GithubSearch/app/src/main/java/com/example/app/presentation/view/detail/RepositoryAdapter.kt
+++ GithubSearch/app/src/main/java/com/example/app/presentation/view/detail/RepositoryAdapter.kt
@@ -1,3 +1,3 @@
 package com.example.app.presentation.view.detail
 
-class DetailFragment
+class RepositoryAdapter
