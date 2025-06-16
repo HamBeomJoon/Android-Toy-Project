87e08b47f122b8f6528380e8b4ed3985c4aaedd4
Ham BeomJoon <hbj0802@naver.com>
2025-06-16 14:01:17 +0900

    feat: 유저 정보 받아오기 datasource 추가

---
+++ GithubSearch/app/src/main/java/com/example/app/data/datasource/UserDataSource.kt
@@ -0,0 +1,10 @@
+package com.example.app.data.datasource
+
+import com.example.app.data.dto.UserResponse
+import com.example.app.data.dto.UsersResponse
+
+interface UserDataSource {
+    suspend fun fetchUsers(): Result<List<UsersResponse>>
+
+    suspend fun getUserByUsername(name: String): Result<UserResponse>
+}
---
+++ GithubSearch/app/src/main/java/com/example/app/data/datasource/UserDataSourceImpl.kt
@@ -0,0 +1,27 @@
+package com.example.app.data.datasource
+
+import com.example.app.data.dto.UserResponse
+import com.example.app.data.dto.UsersResponse
+import com.example.app.data.service.UserService
+
+class UserDataSourceImpl(
+    private val userService: UserService,
+) : UserDataSource {
+    override suspend fun fetchUsers(): Result<List<UsersResponse>> =
+        handleApiCall(
+            errorMessage = "유저들 정보 조회 실패",
+            apiCall = { userService.requestUsers() },
+            transform = { response ->
+                response.body() ?: throw IllegalStateException("응답 바디가 null입니다.")
+            },
+        )
+
+    override suspend fun getUserByUsername(name: String): Result<UserResponse> =
+        handleApiCall(
+            errorMessage = "유저 정보 조회 실패",
+            apiCall = { userService.requestUserByName(name) },
+            transform = { response ->
+                response.body() ?: throw IllegalStateException("응답 바디가 null입니다.")
+            },
+        )
+}
