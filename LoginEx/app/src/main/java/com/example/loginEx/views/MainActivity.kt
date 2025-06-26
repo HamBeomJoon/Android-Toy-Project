135c4bddf3c66c8ff36ba7b5996a07069f6b664c
Ham BeomJoon <hbj0802@naver.com>
2025-06-26 13:47:18 +0900

    feat: 네이버 로그인 토큰 삭제, 로그아웃 코드 추가

--- LoginEx/app/src/main/java/com/example/loginEx/views/MainActivity.kt
+++ LoginEx/app/src/main/java/com/example/loginEx/views/MainActivity.kt
@@ -36,6 +36,42 @@ class MainActivity : AppCompatActivity() {
         initKakao()
         initGoogle()
         initNaver()
+        binding.btnLocalLogin.setOnClickListener {
+            // 네이버 로그인 토큰 삭제, 로그아웃
+//            NidOAuthLogin().callDeleteTokenApi(
+//                object : OAuthLoginCallback {
+//                    override fun onSuccess() {
+//                        // 서버에서 토큰 삭제에 성공한 상태입니다.
+//                    }
+//
+//                    override fun onFailure(
+//                        httpStatus: Int,
+//                        message: String,
+//                    ) {
+//                        // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+//                        // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+//                        Log.d(
+//                            "naver signOut",
+//                            "errorCode: ${NaverIdLoginSDK.getLastErrorCode().code}",
+//                        )
+//                        Log.d(
+//                            "naver signOut",
+//                            "errorDesc: ${NaverIdLoginSDK.getLastErrorDescription()}",
+//                        )
+//                    }
+//
+//                    override fun onError(
+//                        errorCode: Int,
+//                        message: String,
+//                    ) {
+//                        // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+//                        // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+//                        onFailure(errorCode, message)
+//                    }
+//                },
+//            )
+//
+        }
     }
 
     private fun initInsets() {
