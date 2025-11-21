a8293aeb5c6dcb9e5453dbe6a0f9486e9f3bd0e8
Ham BeomJoon <hbj0802@naver.com>
2025-11-21 13:23:17 +0900

    feat: bottom Naviagtion 적용

--- Playground/app/build.gradle.kts
+++ Playground/app/build.gradle.kts
@@ -42,7 +42,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -51,4 +50,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.google.android.material:material:1.11.0")
 }
